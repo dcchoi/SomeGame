e1503acf79f99cd242eaed01ad0e40d927fb4469
ted23p <ted@tacden.net>
2013-07-04 18:44:39 -0400

    scene

--- src/com/example/somegame/Rect.java
+++ src/com/example/somegame/Rect.java
@@ -5,7 +5,7 @@ import org.andengine.opengl.vbo.VertexBufferObjectManager;
 public class Rect extends Rectangle{
 	int rowID;
 	int colID;
-	
+	char mark;
 	public Rect(float pX, float pY, float pWidth, float pHeight,
 			VertexBufferObjectManager pVertexBufferObjectManager,int rowID,int colID) {
 		super(pX, pY, pWidth, pHeight, pVertexBufferObjectManager);
@@ -18,5 +18,9 @@ public class Rect extends Rectangle{
 	
 	void setColID(int n){
 		colID = n; 
+	}
+	
+	void setMarker(char mark){
+		this.mark = mark;
 	}
 }
