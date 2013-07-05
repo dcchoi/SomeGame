d7ebc29f03ce6ddea372ece25a696129fb1eff33
dchoi <dcchoi1234@gmail.com>
2013-07-04 22:56:31 -0400

    broke graphics 1.1

--- src/com/example/somegame/GameScene.java
+++ src/com/example/somegame/GameScene.java
@@ -10,7 +10,8 @@ import com.example.somegame.SceneManager.SceneType;
 
 public class GameScene extends BaseScene{
 	public static boolean flag = true;
-
+	final float xC = 266.6f;
+	final float yC = 160;
 	@Override
 	public void createScene() {
 	    createBackground();	
@@ -41,7 +42,6 @@ public class GameScene extends BaseScene{
 private void createBackground()
 {
 	Sprite bg = new Sprite(0, 0, ResourcesManager.mBackgroundTextureRegion, vbom);
-    bg.setSize(800, 480);
 	setBackground(new SpriteBackground(bg));
     
     
@@ -50,22 +50,26 @@ private void createBackground()
 public void addMarkers(){
 
 		// Create X and O Sprites		
-				final Sprite[][] spO = new Sprite[3][3];
+	Rect[][] rect = new Rect[3][3];
+
+				/*final Sprite[][] spO = new Sprite[3][3];
 				final Sprite[][] spX = new Sprite[3][3];
-				Rect[][] rect = new Rect[3][3];
 
 				for(int i=0; i<3; i++){
 					for(int j=0; j<3; j++){
-						spO[i][j] = new Sprite(i*100,j*100, ResourcesManager.mO,  vbom);
-						spX[i][j] = new Sprite(i*100,j*100, ResourcesManager.mX,  vbom);
+						spO[i][j] = new Sprite(i*xC,j*yC, ResourcesManager.mO,  vbom);
+						spX[i][j] = new Sprite(i*xC,j*yC, ResourcesManager.mX,  vbom);
 
 					}
-					}
-				
+					}*/
+	
+					final Sprite sp = new Sprite(1,1,ResourcesManager.mO,  vbom);
+					final Sprite sp1 = new Sprite(xC/2,0,ResourcesManager.mX,  vbom);
 		 //Create touchevents from rectangles
 				for(int i=0; i<3; i++){
 					for(int j=0; j<3; j++){
-						rect[i][j]= new Rect(i*100,j*100,(i*100)+100,(j*100)+100, vbom,i,j){
+						//rect[i][j]= new Rect(i*100,j*100,(i*100)+100,(j*100)+100, vbom,i,j){
+						rect[i][j]= new Rect(i*xC,j*yC,(i*xC)+xC,(j*yC)+yC, vbom,i,j){
 							boolean tag = true;
 							@Override
 							public boolean onAreaTouched(final TouchEvent pSceneTouchEvent, final float pTouchAreaLocalX, final float pTouchAreaLocalY) {
@@ -75,11 +79,13 @@ public void addMarkers(){
 			                    case TouchEvent.ACTION_DOWN: {    
 			                    	if(tag == true){
 			                    		if(flag){
-			                    		attachChild(spO[rowID][colID]);
+			                    	//	attachChild(spO[rowID][colID]);
+			                    			attachChild(sp);
 			                    		setMarker('O');
 			                    		flag = false;
 			                    		}else{
-			                    		attachChild(spX[rowID][colID]);
+			                    			attachChild(sp1);
+			                    	//	attachChild(spX[rowID][colID]);
 			                    		setMarker('X');
 			                    		flag = true;
 			                    		}
@@ -99,8 +105,8 @@ public void addMarkers(){
 					for(int j=2; j>=0; j--){
 						attachChild(rect[i][j]);
 						registerTouchArea(rect[i][j]);
-						//rect[i][j].setColor(10*k,0,0);
-						//rect[i][j].setAlpha(200);
+						rect[i][j].setColor(10*k,20*k,0);
+						rect[i][j].setAlpha(200);
 						
 						k++;
 					}
